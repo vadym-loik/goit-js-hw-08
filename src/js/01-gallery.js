cd7023171e648f78f1809dad542ccd1e493ec3b6
Vadym <loikfit@gmail.com>
2021-12-25 01:45:25 +0200

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+const lightboxOptions = {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const gallery = new SimpleLightbox('.gallery a', lightboxOptions);
+
+console.log(galleryItems);
 
 console.log(galleryItems);
