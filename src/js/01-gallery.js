04126b944d8cbf94a4908f03e916a6d2b7057798
Vadym <loikfit@gmail.com>
2021-12-28 21:17:36 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,9 +7,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
-
 const galleryList = document.querySelector('.gallery');
 
 const createGalleryMarkup = galleryItems
