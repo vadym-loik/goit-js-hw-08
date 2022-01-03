7f1915812057193f0cdaa79f0fc1fee00bf740c1
Vadym <loikfit@gmail.com>
2022-01-03 02:02:55 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-msg';
+
+const refs = {
+  form: document.querySelector('.js-feedback-form'),
+  textarea: document.querySelector('.js-feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(e) {
+  const message = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    console.log(savedMessage);
+    refs.textarea.value = savedMessage;
+  }
+}
